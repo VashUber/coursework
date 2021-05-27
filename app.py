131192efc87f28a711435b4a778ece54c0977ac6
Egor Sizov <mrjarokvk@gmail.com>
2021-05-27 10:48:11 +0300

    Добавление записей в таблицу

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from enum import unique
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
@@ -19,7 +20,7 @@ class Profiles(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(54), nullable=True)
     old = db.Column(db.Integer)
-    name = db.Column(db.String(90))
+    city= db.Column(db.String(90))
 
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
 
@@ -30,6 +31,18 @@ def home():
 
 @app.route('/registration', methods=("POST", "GET"))
 def registration():
+    if request.method == "POST":
+        try:
+            hash = generate_password_hash(request.form['password'])
+            u = Users(email=request.form['email'], password=hash)
+            db.session.add(u)
+            db.session.flush()
+
+            p = Profiles(name=request.form['name'], old=request.form['old'], city=request.form['city'], user_id=u.id)
+            db.session.add(p)
+            db.session.commit()
+        except:
+            db.session.rollback()
     return render_template('reg.html', title="Регистрация")
 
 if __name__ == '__main__':
