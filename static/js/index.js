f29b4b7d2b7d1dc37c989694014daccb6cfa47a1
Egor Sizov <mrjarokvk@gmail.com>
2021-06-30 19:42:07 +0300

    fix

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,12 @@
-let burger = document.querySelector(".header__burger-menu");
-let menu = document.querySelector(".navigation");
-let body = document.querySelector("body");
-burger.onclick = () => {
+const burger = document.querySelector(".header__burger-menu");
+const menu = document.querySelector(".navigation");
+const body = document.querySelector("body");
+
+const menuActive = function() {
   menu.classList.toggle("navigation--active");
   body.classList.toggle("body--stop-scroll");
   burger.classList.toggle("burger--active");
-};
+}
 
-menu.onclick = () => {
-    menu.classList.toggle("navigation--active");
-    burger.classList.toggle("burger--active");
-};
-
+burger.addEventListener("click", menuActive)
+menu.addEventListener("click", menuActive)
